20fe4f328d9d831838c1ac6fbd7bf2ee3dbdb151
Aria Yudanto <aria.yudanto@gmail.com>
2018-05-19 11:19:51 +0700

    initial commit

---
+++ src/main/java/com/aria/pdf/eform/Main.java
@@ -0,0 +1,104 @@
+package com.aria.pdf.eform;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDMetadata;
+import org.apache.pdfbox.pdmodel.font.*;
+import org.apache.pdfbox.pdmodel.interactive.form.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+    public static final String DEFAULT_FORM_PATH = "/Users/aria/dev/pdf-eform/src/main/resources/form.pdf";
+
+    public static void main(String[] args) throws Exception {
+        String formPath = "";
+        if (args.length != 1)
+            formPath = Main.DEFAULT_FORM_PATH;
+        else
+            formPath = args[0];
+
+
+        Main main = new Main();
+        main.writeField(formPath);
+
+    }
+
+    public void writeField(String formPath) throws Exception {
+        System.out.println("Loading form : " + formPath);
+
+        File formFile = new File(formPath);
+        PDDocument doc = PDDocument.load(formFile);
+        PDPage firstPage = doc.getPage(0);
+        PDFont font = PDType1Font.COURIER;
+
+        PDPageContentStream stream = new PDPageContentStream(
+                doc,
+                firstPage,
+                PDPageContentStream.AppendMode.APPEND,
+                true,
+                true);
+
+        stream.setFont(font, 9);
+
+        stream.beginText();
+        stream.newLineAtOffset(170, 786);
+        stream.showText("34252223242"); // cif
+        stream.endText();
+
+        stream.beginText();
+        stream.newLineAtOffset(170, 771);
+        stream.showText("423784623874432"); // account no
+        stream.endText();
+
+        stream.beginText();
+        stream.newLineAtOffset(170, 756);
+        stream.showText("56363746573749923"); // account no
+        stream.endText();
+
+
+        stream.close();
+
+        doc.save(formPath + ".result.pdf");
+    }
+
+    public void listField(String formPath) throws IOException {
+
+        System.out.println("Loading form : " + formPath);
+
+        File formFile = new File(formPath);
+        PDDocument doc = PDDocument.load(formFile);
+
+        PDDocumentCatalog catalog = doc.getDocumentCatalog();
+        PDMetadata metadata = catalog.getMetadata();
+        PDAcroForm form = catalog.getAcroForm();
+        List fields = form.getFields();
+
+        System.out.println("Fields " + fields.get(0).getClass());
+
+        for (Object field : fields) {
+
+            if (field instanceof PDTextField) {
+                PDTextField textField = (PDTextField) field;
+                System.out.println("Text Field " + textField.getFullyQualifiedName() + " " + textField.getValue());
+            } else if (field instanceof PDChoice) {
+                PDChoice choiceField = (PDChoice) field;
+                System.out.println("Choice Field " + choiceField.getFullyQualifiedName() + " " + choiceField.getValue());
+            } else if (field instanceof PDCheckBox) {
+                PDCheckBox checkBox = (PDCheckBox) field;
+                System.out.println("CheckBox Field " + checkBox.getFullyQualifiedName() + " " + checkBox.getValue());
+            } else {
+                System.out.print(field);
+                System.out.print(" = ");
+                System.out.print(field.getClass());
+                System.out.println();
+            }
+
+        }
+    }
+}
